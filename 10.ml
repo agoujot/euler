f89262ff44db873e760c33d7228a97e17a6a8a26
agoujot <145840578+agoujot@users.noreply.github.com>
2023-10-27 16:06:21 +0200

    ind.cor. 10.ml

--- 10.ml
+++ 10.ml
@@ -1,16 +1,16 @@
 let rec prime (x, l) =
-  if l > int_of_float (sqrt (float_of_int x))
-  then 1
-  else
-    if x mod l = 0 && prime(l, 2) = 1
-    then 0
-    else prime (x, l+1)
+	if l > int_of_float (sqrt (float_of_int x))
+	then 1
+	else
+		if x mod l = 0 && prime(l, 2) = 1
+		then 0
+		else prime (x, l+1)
 let rec so (x, s) =
-    if x = 2000000
-    then s
-    else
-        if prime (x, 2) = 1
-        then so (x+1, s+x)
-        else so (x+1, s)
+	if x = 2000000
+	then s
+	else
+		if prime (x, 2) = 1
+		then so (x+1, s+x)
+		else so (x+1, s)
 let a = so (2, 0)
 let () = print_int a
