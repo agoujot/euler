7620b07b04f69fb13902aa199ddb702da6aa6334
agoujot <145840578+agoujot@users.noreply.github.com>
2023-10-27 12:38:22 +0200

    ind.cor. 4.ml

--- 4.ml
+++ 4.ml
@@ -1,22 +1,22 @@
 let rec pal (s, i) =
-  if i = String.length(s)
-  then true
-  else
-    if s.[i] = s.[String.length(s)-1-i]
-    then pal (s, i+1)
-    else false
+	if i = String.length(s)
+	then true
+	else
+		if s.[i] = s.[String.length(s)-1-i]
+		then pal (s, i+1)
+		else false
 let rec b (l1, l2, h) =
-  if l1 = 1000
-  then 
-    if l1*l2 > h && pal (string_of_int(l1*l2), 0) = true
-    then b (100, l2 + 1, l1*l2)
-    else b (100, l2 + 1, h)
-  else
-    if l2 = 1000
-    then h
-    else 
-      if l1*l2 > h && pal (string_of_int(l1*l2), 0) = true
-      then b (l1+1, l2, l1*l2)
-      else b(l1+1, l2, h)
+	if l1 = 1000
+	then 
+		if l1*l2 > h && pal (string_of_int(l1*l2), 0) = true
+		then b (100, l2 + 1, l1*l2)
+		else b (100, l2 + 1, h)
+	else
+		if l2 = 1000
+		then h
+		else 
+		  if l1*l2 > h && pal (string_of_int(l1*l2), 0) = true
+		  then b (l1+1, l2, l1*l2)
+		  else b(l1+1, l2, h)
 let a = b(100, 100, 0)
 let c = print_int a
