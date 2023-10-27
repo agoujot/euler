fc4411c33ce7572fa3d723d6e628eddf58dc9b59
agoujot <145840578+agoujot@users.noreply.github.com>
2023-10-27 12:33:08 +0200

    7.ml indentation correction
    I'll shorten that to ind.cor.

--- 7.ml
+++ 7.ml
@@ -1,16 +1,16 @@
 let rec prime (x, l) =
-  if l > int_of_float (sqrt (float_of_int x))
-  then 1
-  else
-    if x mod l = 0 && prime(l, 2) = 1
-    then 0
-    else prime (x, l+1)
+	if l > int_of_float (sqrt (float_of_int x))
+	then 1
+	else
+ 		if x mod l = 0 && prime(l, 2) = 1
+		then 0
+		else prime (x, l+1)
 let rec t (x, c, n) =
-  if c = 10001
-  then n
-  else
-    if prime(x, 2) = 1
-    then t (x+1, c+1, x)
-    else t (x+1, c, n)
+	if c = 10001
+	then n
+	else
+    	if prime(x, 2) = 1
+    	then t (x+1, c+1, x)
+    	else t (x+1, c, n)
 let a = t (2, 0, 1)
 let b = print_int a
