b7353508974f0a0b0b24616df8e17eaa43179a26
agoujot <145840578+agoujot@users.noreply.github.com>
2023-10-27 12:45:26 +0200

    ind.cor. 29.ml

--- 29.ml
+++ 29.ml
@@ -3,19 +3,19 @@ let rec inl (x, l) =
 	| [] -> false
 	| i::s -> i = x || inl(x, s)
 let rec prime (x, l) =
-  if l > int_of_float (sqrt(float_of_int x))
-  then true
-  else
-    if x mod l = 0 && prime(l, 2)
-    then false
-    else prime (x, l+1)
+	if l > int_of_float (sqrt(float_of_int x))
+	then true
+	else
+		if x mod l = 0 && prime(l, 2)
+		then false
+		else prime (x, l+1)
 let rec factors (x, l, f)=
-  if x = 1
-  then f
-  else
-    if x mod l = 0 && prime(l, 2)
-    then factors(x/l, l, [l]@f)
-    else factors(x, l+1, f)
+	if x = 1
+	then f
+	else
+		if x mod l = 0 && prime(l, 2)
+		then factors(x/l, l, [l]@f)
+		else factors(x, l+1, f)
 let rec p(l, n) =
 	match n with
 	| 0 -> []
@@ -25,7 +25,8 @@ let rec di(a, b) =
 	match (a, b) with
 	| (101, _) -> []
 	| (_, 101) -> di(a+1, 2)
-	| (_, _) -> begin
+	| (_, _) -> 
+ 		begin
 		let v = tri(p(factors(a, 2, []), b))
 		and w = di(a, b+1) in
 		if inl(v, w)
