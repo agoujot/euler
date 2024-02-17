6d610002fccef577349f81290aebaa6fccfa96c7
agoujot <145840578+agoujot@users.noreply.github.com>
2024-02-17 21:09:07 +0100

    Update 26.ml

--- 26.ml
+++ 26.ml
@@ -1,14 +1,11 @@
-let rec inl (x, l)  =
-	match l with
-	| [] -> false
-	| i::s -> i=x || inl(x, s)
+
 let rec cyc (r, d, p) =
 	if r = 0
 	then 0
 	else
 		let vs = r*10 in
 		let nr = vs mod d in
-		if not (inl(nr, p))
+		if not (List.mem nr p))
 		then cyc(nr, d, [nr]@p)
 		else List.length p
 let cy x = cyc (1 mod x, x, [1 mod x])
