88bc41f31f0a7e6a2dd2e96eeb0c0def0bed50dd
agoujot <145840578+agoujot@users.noreply.github.com>
2024-02-17 21:10:37 +0100

    Update 29.ml

--- 29.ml
+++ 29.ml
@@ -1,21 +1,4 @@
-let rec inl (x, l) =
-	match l with
-	| [] -> false
-	| i::s -> i = x || inl(x, s)
-let rec prime (x, l) =
-	if l > int_of_float (sqrt(float_of_int x))
-	then true
-	else
-		if x mod l = 0 && prime(l, 2)
-		then false
-		else prime (x, l+1)
-let rec factors (x, l, f)=
-	if x = 1
-	then f
-	else
-		if x mod l = 0 && prime(l, 2)
-		then factors(x/l, l, [l]@f)
-		else factors(x, l+1, f)
+open Lib
 let rec p(l, n) =
 	match n with
 	| 0 -> []
@@ -29,7 +12,7 @@ let rec di(a, b) =
  		begin
 		let v = tri(p(factors(a, 2, []), b))
 		and w = di(a, b+1) in
-		if inl(v, w)
+		if List.mem v w
 		then w
 		else [v]@w
 		end
