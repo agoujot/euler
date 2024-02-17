bcac5038e8295d7dbb35b2e17f103293374620a7
agoujot <145840578+agoujot@users.noreply.github.com>
2024-02-17 21:14:55 +0100

    Update 38.lua

--- 38.lua
+++ 38.lua
@@ -1,20 +1,4 @@
-function get(st, i)
-	return st:byte(i)-48
-end
-function pt(t) for i = 1, #t do print(t[i]) end end
-function pandigital(st)
-	local t = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	for i = 1, #st do 
-		t[get(st, i)+1] = t[get(st, i)+1] + 1
-	end
-	local ok = t[1] == 0;
-	for i = 2, 10 do 
-		if t[i] ~= 1 then ok = false end
-	end
-	return ok
-end
-most = 0;
-assert(pandigital("123456789"))
+require "lib";
 for x = 1, 10000 do
 	s = "";
 	for n = 1, 8 do
