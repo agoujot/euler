de01dd67c0c3c3eb604012b4037c94142121da53
agoujot <145840578+agoujot@users.noreply.github.com>
2024-02-17 21:12:28 +0100

    Update 33.ml

--- 33.ml
+++ 33.ml
@@ -1,37 +1,5 @@
 open List
-let rec factors (x, l, f)=
-	if x = 1
-	then f
-	else
-		if x mod l = 0 && prime(l, 2) = 1
-		then factors(x/l, l, [l]@f)
-		else factors(x, l+1, f)
-and prime (x, l)=
-	if l > int_of_float (sqrt (float_of_int x))
-	then 1
-	else
-		if x mod l = 0 && prime(l, 2) = 1
-		then 0
-		else prime (x, l+1)
-and red(fa, fb, i) =
-	if i = length fa || i = length fb
-	then [ti(fa); ti(fb)]
-	else 
-		if inl(nth fa i, fb)
-		then red(rem((nth fa i), fa, []), rem((nth fa i), fb, []), i)
-		else red(fa, fb, i+1)
-and inl (x, l) =
-  match l with
-  | [] -> false
-  | i::s -> i = x || inl (x, s)
-and ti(l) =
-	match l with
-	| [] -> 1
-	| i::s -> i*ti(s)
-and rem(x, l, n) =
-	match l with
-	| [] -> n
-	| i::s -> if i = x then n@s else rem(x, s, n@[i])
+open Lib
 let rec p(a, b) =
 	if a = 100
 	then p(10, b+1)
